1eaab7c1b93cd7d46464833eddc682e2534f5ab5
Lena Hou <lenahou2013@gmail.com>
2021-01-25 15:33:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def move(board, index, token = "X")
   board[index] = token
 end
 
-def turn(user_input, board, index)
+def turn(user_input)
 puts "Please enter 1-9:"
 user_input = gets.strip
 input_to_index(user_input)
